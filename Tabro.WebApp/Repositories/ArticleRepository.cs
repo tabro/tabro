230a92db7b4b66cfb6c155e4888c2c6b26423eb0
Lars Tabro Sørensen <lars@tabro.dk>
2015-04-01 15:59:17 +0200

    Switched to EventFlow

--- Tabro.WebApp/Repositories/ArticleRepository.cs
+++ Tabro.WebApp/Repositories/ArticleRepository.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using EventFlow.ReadStores.InMemory;
 using Tabro.Domain.Article;
 
 namespace Tabro.WebApp.Repositories
 {
     public class ArticleRepository : IArticleRepository
     {
+        private readonly IInMemoryReadModelStore<ArticleAggregate, ArticleView> _articleReadModelStore;
+
+        public ArticleRepository(IInMemoryReadModelStore<ArticleAggregate, ArticleView> articleReadModelStore)
+        {
+            _articleReadModelStore = articleReadModelStore;
+        }
+
         public List<ArticleView> GetAll()
         {
-            return null;
+            return new List<ArticleView>
+            {
+                _articleReadModelStore.Get("test")
+            }.Where(x => x != null).ToList();
         }
 
         public ArticleView GetByArticleKey(ArticleKey key)
         {
-            return null;
+            return new ArticleView();
         }
     }
 }
